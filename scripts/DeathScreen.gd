074bd9bd55495d3134435175b03e5266349c8639
minerscale <minerscale@users.noreply.github.com>
2017-04-23 21:30:07 +1000

    Final commit
    Signed-off-by: minerscale <minerscale@users.noreply.github.com>

--- scripts/DeathScreen.gd
+++ scripts/DeathScreen.gd
@@ -3,7 +3,10 @@ extends Label
 var time = Globals.get("TIME")
 
 func _ready():
-	set_text("You suck! You only lasted " + str(int(time)) + " seconds!")
+	if (time < 60):
+		set_text("You suck! You only lasted " + str(int(time)) + " seconds!")
+	else:
+		set_text("You win! You lasted " + str(int(time)) + " seconds! :) :)")
 
 func _on_Button_pressed():
 	get_node("/root/globals").setScene("res://scenes/main.tscn")
